11e9b5de1d98d26dfabde028be456c8c71255296
eileenhsiao <s111219026@stu.ntue.edu.tw>
2025-03-12 17:34:21 +0800

    update

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+import ProductList from '../components/ProductList'
+
+function Home(){
+    return(
+        <div>
+            <Header />
+            <ProductList />
+            <Footer />
+        </div>
+
+    )
+
+}
+
+export default Home
